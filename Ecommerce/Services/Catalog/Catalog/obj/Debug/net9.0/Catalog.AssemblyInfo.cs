45e7642bc0e46fa7bc0cd24ec483d88753448bbd
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-04 12:56:58 +0700

    Backend_Catalog_Microservice: Add Product, ProductBrand, and ProductType entities

--- Ecommerce/Services/Catalog/Catalog/obj/Debug/net9.0/Catalog.AssemblyInfo.cs
+++ Ecommerce/Services/Catalog/Catalog/obj/Debug/net9.0/Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce8651fb2674f90eb5426c8f50e68860a70326c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9994b56e358617421e0d305884113bf7afc3585")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
