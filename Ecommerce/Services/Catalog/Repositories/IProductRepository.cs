59f3fdb1c41b0e02af914302276d0d171ccc26ec
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-05 10:43:49 +0700

    Backend_Catalog_Microservice: add interface Product repository

---
+++ Ecommerce/Services/Catalog/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
+        Task<IEnumerable<Product>> GetProductsByBrandAsync(string brand);
+        Task<Product> GetProductByIdAsync(Guid id);
+        Task<Product> CreateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(Guid id);
+        Task<bool> UpdateProductAsync(Product product);
+
+    }
+}
