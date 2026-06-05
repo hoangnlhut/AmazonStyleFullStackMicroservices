ea687d7f7118148a19ff9e9499a0193416dfc3d8
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-05 10:50:04 +0700

    Backend_Catalog_Microservice: create pagination class and add new action in interfae product repository

--- Ecommerce/Services/Catalog/Repositories/IProductRepository.cs
+++ Ecommerce/Services/Catalog/Repositories/IProductRepository.cs
@@ -1,10 +1,12 @@
 ﻿using Catalog.Entities;
+using Catalog.Specifications;
 
 namespace Catalog.Repositories
 {
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Pagination<Product>> GetProductsAsync(CatalogSpecParams specParams);
         Task<IEnumerable<Product>> GetProductsByNameAsync(string name);
         Task<IEnumerable<Product>> GetProductsByBrandAsync(string brand);
         Task<Product> GetProductByIdAsync(Guid id);
