1375cc5704f33dd2faba6cf2407371b367fa69d0
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-05 15:39:05 +0700

    Backend_Catalog_Microservice: define Pagination (Result) and CatalogSpecParams (Input) usomg specification pattern

--- Ecommerce/Services/Catalog/Specifications/CatalogSpecParams.cs
+++ Ecommerce/Services/Catalog/Specifications/CatalogSpecParams.cs
@@ -6,7 +6,7 @@
         private const int MAX_PAGE_SIZE = 70;
         private int _pageSize = 10;
 
-        public int Pageindex { get; set; } = 1;
+        public int PageIndex { get; set; } = 1;
         public int PageSize { 
             get => _pageSize; 
             set => _pageSize = (value > MAX_PAGE_SIZE) ? MAX_PAGE_SIZE : value;
--- Ecommerce/Services/Catalog/Specifications/Pagination.cs
+++ Ecommerce/Services/Catalog/Specifications/Pagination.cs
@@ -2,12 +2,12 @@
 {
     public class Pagination<T> where T : class
     {
-        public Pagination()
+        public Pagination() 
         {
             
         }
 
-        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyCollection<T> data)
+        public Pagination(int pageIndex, int pageSize, long count, IReadOnlyCollection<T> data)
         {
             PageIndex = pageIndex;
             PageSize = pageSize;
@@ -17,7 +17,7 @@
 
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
-        public int Count { get; set; }
+        public long Count { get; set; }
         public IReadOnlyCollection<T> Data { get; set; }
     }
 }
