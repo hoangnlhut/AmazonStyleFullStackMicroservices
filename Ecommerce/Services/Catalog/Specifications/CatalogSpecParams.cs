2ca565b6fc7358bb93a8d29037334a5667e3b9f2
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-05 10:32:34 +0700

    Backend_Catalog_Microservice
    Refactor project structure and add filtering feature
    
    Significant restructuring of the project:
    - Updated `.gitignore` to ignore debug object files.
    - Removed redundant `.csproj`-related NuGet and build files.
    - Simplified solution structure by modifying `Ecommerce.slnx`.
    - Added `CatalogSpecParams.cs` for catalog item filtering with pagination, sorting, and search capabilities.
    - Updated `DocumentLayout.json` to reflect new file paths and added references to new files.
    - Removed auto-generated and cache files to clean up the repository.
    
    These changes streamline the project structure and introduce a new filtering feature for catalog items.

---
+++ Ecommerce/Services/Catalog/Specifications/CatalogSpecParams.cs
@@ -0,0 +1,20 @@
+﻿namespace Catalog.Specifications
+{
+    //filtering container for the catalog items, it will be used in the specification pattern to filter the items based on the parameters provided by the user
+    public class CatalogSpecParams
+    {
+        private const int MAX_PAGE_SIZE = 70;
+        private int _pageSize = 10;
+
+        public int Pageindex { get; set; } = 1;
+        public int PageSize { 
+            get => _pageSize; 
+            set => _pageSize = (value > MAX_PAGE_SIZE) ? MAX_PAGE_SIZE : value;
+        }
+
+        public string? BrandId { get; set; }
+        public string? TypeId { get; set; }
+        public string? Sort { get; set; }
+        public string? Search { get; set; }
+    }
+}
