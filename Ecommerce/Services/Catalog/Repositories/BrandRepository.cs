b5f7c392f48999e4283ab6540200511ba7c9fadc
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-05 15:39:36 +0700

    Backend_Catalog_Microservice: Implement Brand repository

---
+++ Ecommerce/Services/Catalog/Repositories/BrandRepository.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Entities;
+using Catalog.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class BrandRepository : IBrandRepository
+    {
+        private readonly IMongoCollection<ProductBrand> _brandCollection;
+        public BrandRepository(IOptions<CatalogDatabaseSettings> catalogDatabaseSettings)
+        {
+            _brandCollection = new MongoClient(catalogDatabaseSettings.Value.ConnectionString)
+            .GetDatabase(catalogDatabaseSettings.Value.DatabaseName)
+            .GetCollection<ProductBrand>(catalogDatabaseSettings.Value.BrandCollectionName);
+        }
+
+        public async Task<IEnumerable<ProductBrand>> GetBrandsAsync()
+        {
+            return await _brandCollection.Find(_ => true).ToListAsync();
+        }
+        public async Task<ProductBrand> GetBrandByIdAsync(string id)
+        {
+            return await _brandCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+        }
+        public async Task CreateBrandAsync(ProductBrand brand)
+        {
+            await _brandCollection.InsertOneAsync(brand);
+        }
+        public async Task UpdateBrandAsync(string id, ProductBrand brand)
+        {
+            await _brandCollection.ReplaceOneAsync(x => x.Id == id, brand);
+        }
+        public async Task DeleteBrandAsync(string id)
+        {
+            await _brandCollection.DeleteOneAsync(x => x.Id == id);
+        }
+    }
+}
