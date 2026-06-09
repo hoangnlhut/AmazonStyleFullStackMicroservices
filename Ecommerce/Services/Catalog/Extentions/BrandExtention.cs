f8e9fbd5c2ab5bc84aab12febe0b21e6b0ee90cc
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-09 10:43:49 +0700

    Backend_Catalog_Microservice:Creating Get A;ll Brands query Handler

---
+++ Ecommerce/Services/Catalog/Extentions/BrandExtention.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.Entities;
+using Catalog.Responses;
+
+namespace Catalog.Extentions
+{
+    public static class BrandExtention
+    {
+        public static IEnumerable<BrandResponse> ToResponseList(this IEnumerable<ProductBrand> brands)
+        {
+            return brands.Select(brand => brand.ToResponse()).ToList();
+        }
+
+        public static BrandResponse ToResponse(this ProductBrand brand)
+        {
+            return new BrandResponse
+            {
+                Id = brand.Id,
+                Name = brand.Name
+            };
+        }
+    }
+}
+ 
---
+++ Ecommerce/Services/Catalog/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Extentions;
+using Catalog.Queries;
+using Catalog.Repositories;
+using Catalog.Responses;
+using MediatR;
+
+namespace Catalog.Handlers
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IEnumerable<BrandResponse>>
+    {
+        private readonly IBrandRepository _brandRepository;
+
+        public GetAllBrandsHandler(IBrandRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+
+        public async Task<IEnumerable<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            var brandList = await _brandRepository.GetBrandsAsync();
+            return brandList.ToResponseList() ?? Enumerable.Empty<BrandResponse>();
+        }
+    }
+}
