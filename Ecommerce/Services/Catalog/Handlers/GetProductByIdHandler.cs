189166e0a1bac255cc47b0fef638c3878b27fb97
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-09 20:49:48 +0700

    Backend_Catalog_Microservice:  Get product by Id query and handler

---
+++ Ecommerce/Services/Catalog/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Mappers;
+using Catalog.Queries;
+using Catalog.Repositories;
+using Catalog.Responses;
+using MediatR;
+
+namespace Catalog.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery, ProductResponse>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            // Implementation for retrieving the product by ID
+            var product = await _productRepository.GetProductByIdAsync(request.id);
+            return product.ToResponse();
+        }
+    }
+}
