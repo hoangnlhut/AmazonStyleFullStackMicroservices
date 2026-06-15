c61711f9369015a4f3484fb34de936b3b21f1932
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-15 15:21:56 +0700

    Backend_Basket_Microservice: define Shopping cart and Shopping cart item Entities

---
+++ Ecommerce/Services/Basket/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.Entities
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
