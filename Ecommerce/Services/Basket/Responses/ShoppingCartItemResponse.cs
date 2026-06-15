7704d22666904e469cfe4e15306ca5f91c359f97
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-15 22:14:37 +0700

    Backend_Basket_Microservice: create Shopping Cart and Item Response

---
+++ Ecommerce/Services/Basket/Responses/ShoppingCartItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.Responses
+{
+    public record ShoppingCartItemResponse(
+        string ProductId,
+        string ProductName,
+        string ImageFile,
+        decimal Price,
+        int Quantity
+    );
+}
---
+++ Ecommerce/Services/Basket/Responses/ShoppingCartResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.Responses
+{
+    public record ShoppingCartResponse(string UserName, List<ShoppingCartItemResponse> Items)
+    {
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+        public ShoppingCartResponse(string userName) : this(userName, new List<ShoppingCartItemResponse>())
+        {
+            
+        }
+    }
+}
