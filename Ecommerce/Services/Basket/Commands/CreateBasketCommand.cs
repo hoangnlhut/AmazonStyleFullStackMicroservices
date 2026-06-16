3e3f259da896242ccfc1ceb42039b8ddcab44dae
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-16 15:13:16 +0700

    Backend_Basket_Microservice: Creating Shopping Cart Command and DTO

---
+++ Ecommerce/Services/Basket/Commands/CreateBasketCommand.cs
@@ -0,0 +1,10 @@
+﻿using Basket.DTOs;
+using Basket.Responses;
+using MediatR;
+
+namespace Basket.Commands
+{
+    public record CreateBasketCommand(string UserName, List<CreateShoppingCartItemDto> Items) : IRequest<ShoppingCartResponse>
+    {
+    }
+}
