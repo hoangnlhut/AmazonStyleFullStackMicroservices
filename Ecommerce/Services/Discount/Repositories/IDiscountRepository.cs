1abe886de232ea3c202d8ae1200851839640a31b
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-19 13:51:32 +0700

    Backend_Discount_Microservice: Creating Discount Repository

---
+++ Ecommerce/Services/Discount/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Entities;
+
+namespace Discount.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetCoupon(string productName);
+        Task<bool> CreateCoupon(Coupon coupon);
+        Task<bool> UpdateCoupon(int id, Coupon coupon);
+        Task<bool> DeleteCoupon(string productName);
+    }
+}
