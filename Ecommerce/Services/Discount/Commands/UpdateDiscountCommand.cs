f63efa35907a858a09a62b9ddcef3cf19eba1013
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-22 14:10:40 +0700

    Backend_Discount_Microservice: create and implement UpdateDisount Command

---
+++ Ecommerce/Services/Discount/Commands/UpdateDiscountCommand.cs
@@ -0,0 +1,7 @@
+﻿using Discount.DTOs;
+using MediatR;
+
+namespace Discount.Commands
+{
+    public record UpdateDiscountCommand(CouponInput couponInput) : IRequest<bool>;
+}
---
+++ Ecommerce/Services/Discount/Handlers/Commands/UpdateDiscountHandle.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Commands;
+using Discount.Mappers;
+using Discount.Repositories;
+using MediatR;
+
+namespace Discount.Handlers.Commands
+{
+    public class UpdateDiscountHandle : IRequestHandler<UpdateDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public UpdateDiscountHandle(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        public async Task<bool> Handle(UpdateDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var coupon = request.couponInput.ToEntity();
+            var result = await _discountRepository.UpdateCoupon(coupon);
+            return result;
+        }
+    }
+}
