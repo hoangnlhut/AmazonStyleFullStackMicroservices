04173ff7ce449ca289322867cf2f11631c9b150f
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-22 14:15:43 +0700

    Backend_Discount_Microservice: create and implement DeleteDisount Command

---
+++ Ecommerce/Services/Discount/Commands/DeleteDiscountCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Discount.Commands
+{
+    public record DeleteDiscountCommand(string productName) : IRequest<bool>;
+}
---
+++ Ecommerce/Services/Discount/Handlers/Commands/DeleteDiscountHandle.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Commands;
+using Discount.Repositories;
+using MediatR;
+
+namespace Discount.Handlers.Commands
+{
+    public class DeleteDiscountHandle : IRequestHandler<DeleteDiscountCommand, bool>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DeleteDiscountHandle(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        public async Task<bool> Handle(DeleteDiscountCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _discountRepository.DeleteCoupon(request.productName);
+            return result;
+        }
+    }
+}
