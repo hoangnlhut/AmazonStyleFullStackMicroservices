0697f18d0e939d27301df4682c4101789e0017d6
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-22 13:57:38 +0700

    Backend_Discount_Microservice: implement GetDisountByProductName Query

---
+++ Ecommerce/Services/Discount/Handlers/Queries/GetDiscountByProductNameHandle.cs
@@ -0,0 +1,24 @@
+﻿using Discount.DTOs;
+using Discount.Mappers;
+using Discount.Queries;
+using Discount.Repositories;
+using MediatR;
+
+namespace Discount.Handlers.Queries
+{
+    public class GetDiscountByProductNameHandle : IRequestHandler<GetDiscountByProductNameQuery, CouponDto>
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public GetDiscountByProductNameHandle(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        public async Task<CouponDto> Handle(GetDiscountByProductNameQuery request, CancellationToken cancellationToken)
+        {
+            var coupon = await _discountRepository.GetCouponByProductName(request.productName);
+            return coupon.ToDto();
+        }
+    }
+}
---
+++ Ecommerce/Services/Discount/Mappers/DiscountMapper.cs
@@ -0,0 +1,18 @@
+﻿using Discount.DTOs;
+using Discount.Entities;
+
+namespace Discount.Mappers
+{
+    public static class DiscountMapper
+    {
+        public static CouponDto ToDto(this Coupon coupon)
+        {
+            return new CouponDto(coupon.Id, coupon.ProductName, coupon.Description, coupon.Amount);
+        }
+
+        //public static Coupon ToEntity(this CouponDto couponDto)
+        //{
+        //    return new Coupon(couponDto.Id, couponDto.ProductName, couponDto.Description, couponDto.Amount);
+        //}
+    }
+}
