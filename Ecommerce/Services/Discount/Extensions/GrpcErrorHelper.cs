ab1acc0d324be2f50fae7a97b15f0eca3f9bb5f9
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-24 11:28:59 +0700

    Backend_Discount_Microservice:  adding custom exception Grpc

---
+++ Ecommerce/Services/Discount/Extensions/GrpcErrorHelper.cs
@@ -0,0 +1,43 @@
+﻿using Google.Protobuf;
+using Google.Protobuf.WellKnownTypes;
+using Google.Rpc;
+using Grpc.Core;
+
+namespace Discount.Extensions
+{
+    public static class GrpcErrorHelper
+    {
+
+        public static RpcException CreateRpcValidationException(Dictionary<string, string> fieldErrors)
+        {
+            var fieldViolations = new List<BadRequest.Types.FieldViolation>();
+
+            foreach (var error in fieldErrors)
+            {
+                fieldViolations.Add(new BadRequest.Types.FieldViolation
+                {
+                    Field = error.Key,
+                    Description = error.Value
+                });
+            }
+
+            //now add bad request
+            var badRequest = new BadRequest();
+            badRequest.FieldViolations.AddRange(fieldViolations);
+
+            var status = new Google.Rpc.Status()
+            {
+                Code = (int)StatusCode.InvalidArgument,
+                Message = "Validation failed for the request.",
+                Details = { Any.Pack(badRequest) }
+            };
+
+            var trailers = new Metadata
+            {
+                { "grpc-status-details-bin", status.ToByteArray() }
+            };
+
+            return new RpcException(new Grpc.Core.Status(StatusCode.InvalidArgument, status.Message), trailers);
+        }
+    }
+}
