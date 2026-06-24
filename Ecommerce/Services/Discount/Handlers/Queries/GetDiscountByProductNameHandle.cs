a08b8da74b63b673da468b4932de96b6059d0e2c
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-06-24 15:07:34 +0700

    Backend_Discount_Microservice: using Custom Exception

--- Ecommerce/Services/Discount/Handlers/Queries/GetDiscountByProductNameHandle.cs
+++ Ecommerce/Services/Discount/Handlers/Queries/GetDiscountByProductNameHandle.cs
@@ -18,11 +18,23 @@ namespace Discount.Handlers.Queries
 
         public async Task<CouponDto> Handle(GetDiscountByProductNameQuery request, CancellationToken cancellationToken)
         {
+            //Validate the input
+            if(string.IsNullOrWhiteSpace(request.productName))
+            {
+                var validationErrors = new Dictionary<string, string>
+                {
+                    { "ProductName", "Product name must not be empty" }
+                };
+            }
+
+            //fetch from repo
             var coupon = await _discountRepository.GetCouponByProductName(request.productName);
             if (coupon == null || coupon.Id == 0) 
             {
                 throw new RpcException(new Status(StatusCode.NotFound, $"Discount not found for the product name {request.productName}"));
             }
+
+            //mapping
             return coupon.ToDto();
         }
     }
