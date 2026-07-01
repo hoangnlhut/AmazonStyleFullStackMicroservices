f2762810c96514f2afacc4db39b0783b8f90dc49
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-07-01 11:10:21 +0700

    Backend_Order_Microservice: create entiies : base class and order entity

---
+++ Ecommerce/Services/Ordering/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Entities
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+
+        //Few audit props
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
