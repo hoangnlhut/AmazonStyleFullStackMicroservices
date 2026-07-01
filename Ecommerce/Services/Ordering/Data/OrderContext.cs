6f50fa93057b859a51ca65be8d47f61af6d26337
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-07-01 12:56:52 +0700

    Backend_Order_Microservice: Create Order Context class

---
+++ Ecommerce/Services/Ordering/Data/OrderContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Entities;
+
+namespace Ordering.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
