38c3e092d2d9ef5738dcbc5afd69ce576e82c657
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-07-03 09:17:42 +0700

    Backend_Order_Microservice: Creating OrderDto

---
+++ Ecommerce/Services/Ordering/DTOs/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.DTOs
+{
+    record OrderDto
+    (
+        int Id,
+        string UserName,
+        decimal TotalPrice,
+        string FirstName,
+        string LastName,
+        string EmailAddress,
+        string AddressLine,
+        string Country,
+        string State,
+        string ZipCode,
+        string CardName,
+        string CardNumber,
+        string Expiration,
+        string Cw,
+        int PaymentMethod
+    );
+}
