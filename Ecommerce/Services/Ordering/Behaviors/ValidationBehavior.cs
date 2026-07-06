afd78cacf3be3e2cc0d38c636eb134e27a4d61d1
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-07-06 18:28:41 +0700

    Backend_Order_Microservice: Creating validator behavior middleware

---
+++ Ecommerce/Services/Ordering/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Ordering.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;   
+        }
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                //Run all the validation one by one and return the VAlidation Result
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                //now need to check for any failures
+                var failures = validationResults.SelectMany(e => e.Errors).Where(f => f != null).ToList();
+                if(failures.Count > 0)
+                {
+                    throw new ValidationException(failures);
+                }
+            }
+
+            return await next();
+        }
+    }
+}
