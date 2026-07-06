a976c27474453564ccca11937cf769649e31d621
hoangnlhut <hoangnguyenle.hnlhut@gmail.com>
2026-07-06 21:02:58 +0700

    Backend_Order_Microservice: Creating Middleware to handle the unhandled Exception behavior
    Add UnhandleExceptionBehavior for exception logging
    A new `UnhandleExceptionBehavior<TRequest, TResponse>` class was added to the `Ordering.Behaviors` namespace. This class implements the `IPipelineBehavior<TRequest, TResponse>` interface from MediatR to handle unhandled exceptions during request processing.
    The `Handle` method wraps the execution of the next delegate in a try-catch block. If an exception occurs, it logs the exception details, including the request name and the request object, and then rethrows the exception. This improves observability and debugging capabilities by ensuring detailed logging of unhandled exceptions.

---
+++ Ecommerce/Services/Ordering/Behaviors/UnhandleExceptionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+
+namespace Ordering.Behaviors
+{
+    public class UnhandleExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandleExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
